18da7786f430aa859ec8c858497e665f9637a64f
sergiosaraivajr <sergiosaraivajr01@gmail.com>
2024-10-15 15:12:01 -0300

    BUILD: definição do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import "./App.css";
+import React from "react";
+// import "./App.css";
 
 function App() {
   return (
-    <>
-      <main></main>
-    </>
+    <main>
+      <header className="bg-green-800 shadow-gray-700 shadow-lg h-12 flex items-center justify-center">
+        <h1 className="text-green-200 w-auto font-bold">
+          Minha Galeria de Fotos
+        </h1>
+      </header>
+    </main>
   );
 }
 
